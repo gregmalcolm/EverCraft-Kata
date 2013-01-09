c3754f2e017d1ee6fc35cf3fe087aaf1dc695cf1
gregmalcolm <greg@gregmalcolm.com>
2013-01-09 14:52:35 -0500

    We can now beat our character up

--- ruby/lib/character.rb
+++ ruby/lib/character.rb
@@ -1,10 +1,14 @@
 class Character
   attr_accessor :name
   attr_reader :alignment
+  attr_reader :hp
+  attr_reader :armor_class
 
   def initialize
     @name = "Gandalf"
     @alignment = :neutral
+    @hp = 5
+    @armor_class = 10
   end
 
   def alignment=(alignment)
@@ -12,6 +16,17 @@ class Character
       raise "Alignment #{alignment} is not supported"
     end
     @alignment = alignment
+  end
+
+  def take_damage(damage = 1, options = {})
+    if options[:critical] == true
+      damage *= 2
+    end
+    @hp -= damage
+  end
+
+  def dead?
+    @hp <= 0
   end
 
 end
--- ruby/spec/character_spec.rb
+++ ruby/spec/character_spec.rb
@@ -34,4 +34,31 @@ describe Character do
     lambda { subject.alignment = :bad }.should raise_error(/not supported/)
   end
 
+  it "has a default armor class of 10" do
+    subject.armor_class.should == 10
+  end
+
+  it "has a default hp of 5" do
+    subject.hp.should == 5
+  end
+
+  it "takes 1 damage" do
+    subject.take_damage
+    subject.hp.should == 4
+  end
+
+  it "takes double damage for a critical hit" do
+    subject.take_damage(2, :critical => true)
+    subject.hp.should == 1
+  end
+
+  it "is dead when hp is less than 0" do
+    subject.take_damage(10)
+    subject.dead?.should be_true
+  end
+
+  it "is alive when hp is more than 0" do
+    subject.take_damage(1)
+    subject.dead?.should be_false
+  end
 end
