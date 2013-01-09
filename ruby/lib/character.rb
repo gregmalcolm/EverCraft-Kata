604c670ca0514395d92c1d97e07c81e8f33c6127
gregmalcolm <greg@gregmalcolm.com>
2013-01-09 14:23:26 -0500

    We can now be evil

--- ruby/lib/character.rb
+++ ruby/lib/character.rb
@@ -1,8 +1,17 @@
 class Character
   attr_accessor :name
+  attr_reader :alignment
 
   def initialize
     @name = "Gandalf"
+    @alignment = :neutral
+  end
+
+  def alignment=(alignment)
+    unless [:good, :evil, :neutral].include?(alignment)
+      raise "Alignment #{alignment} is not supported"
+    end
+    @alignment = alignment
   end
 
 end
--- ruby/spec/character_spec.rb
+++ ruby/spec/character_spec.rb
@@ -10,4 +10,28 @@ describe Character do
   it "can confirm the name is Gandalf" do
     subject.name.should == "Gandalf"
   end
+
+  it "can set the alignment to good" do
+    subject.alignment = :good
+    subject.alignment.should == :good
+  end
+
+  it "can set the alignment to neutral" do
+    subject.alignment = :neutral
+    subject.alignment.should == :neutral
+  end
+
+  it "can set the alignment to evil" do
+    subject.alignment = :evil
+    subject.alignment.should == :evil
+  end
+
+  it "has a default alignment of neutral" do
+    subject.alignment.should == :neutral
+  end
+
+  it "will raise an exception when alignment is set improperly" do
+    lambda { subject.alignment = :bad }.should raise_error(/not supported/)
+  end
+
 end
